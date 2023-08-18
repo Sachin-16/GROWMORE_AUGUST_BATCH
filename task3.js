e976b44e8ffe584edc6801c2c2a6330c518a60af
sachin singh <115525697+Sachin-16@users.noreply.github.com>
2023-08-18 18:50:59 +0530

    LGMVIP-WEB-TASK-3

---
+++ task3.js
@@ -0,0 +1,18 @@
+
+function takevalue(){
+    var name=document.getElementById("name").value;
+   
+
+     var number=document.getElementById("mobile no.").value;
+   
+
+     var password=document.getElementById("password").value;
+   
+     document.write("your name is = "+ name +"<br>" +"<br>");
+
+      document.write("your number is = "+ number +"<br>" +"<br>");
+
+       document.write("your password is = "+ password +"<br>" +"<br>");
+
+}
+
